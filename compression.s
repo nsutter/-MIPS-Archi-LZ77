6ddd40210a45b8b7b2771a4a00a6da375aa25299
nsutter <nsutter@etu.unistra.fr>
2015-12-05 18:39:32 +0100

    ajout de la fonction pour la creation du fichier et de la fonction d'ecriture dans un fichier

--- compression.s
+++ compression.s
@@ -1,11 +1,27 @@
 .data
 filename: .asciiz "./Pirouette.txt" # nom du fichier
+cre: .asciiz "./test.Z77" # nom du fichier
 textSpace: .space 1050
 
 .text
 #N=11 F=5
 li $t0 11
 li $t1 5
+
+create:				#creer le fichier avec le nom cre
+	li $v0, 13
+	la $a0, cre
+	li $a1, 1
+	li $a2, 0
+	syscall
+	move $t3, $v0
+
+write:				#ecrit dans le fichier lz77 le 10 caractères dans $s1
+	li $v0, 15
+	move $a0, $t3
+	la $a1, $s1
+	li $a2, 10
+	syscall
 
 #Ouverture du fichier
 li $v0 13
