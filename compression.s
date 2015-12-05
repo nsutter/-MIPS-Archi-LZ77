a6719f28f11d96339f0565a25feb027b5f30a0bf
Théo Poizat <theo.poizat@etu.unistra.fr>
2015-12-05 19:02:53 +0100

    Sauvegarde en pile pour fononction Lecture

--- compression.s
+++ compression.s
@@ -5,14 +5,14 @@ saut_ligne: .asciiz "\n"
 toast: .asciiz "\n------------\n"
 
 buffer: .space 1050
-cre: .asciiz "./test.lz77" # nom du fichier écrit
+cre: .asciiz "./test.lz77" # nom du fichier ecrit
 
 .text
 #N=11 F=5
 li $t0 11
 li $t1 5
 
-# Créer un fichier avec le nom cre
+# Creer un fichier avec le nom cre
 create:
 	li $v0, 13
 	la $a0, cre
@@ -21,7 +21,7 @@ create:
 	syscall
 	move $t3, $v0
 
-# Écrit dans le fichier lz77 le 10 caractères dans $s1
+# Ecrit dans le fichier lz77 le 10 caracteres dans $s1
 write:
 	li $v0, 15
 	move $a0, $t3
@@ -56,14 +56,24 @@ j exit
 # Lecture d'un fichier
 # a2 la taille
 Lecture:
-move $a0, $t2
-li $v0, 14
-la $a1, buffer
-syscall  
-la $a0, buffer
-li $v0, 4
-syscall
-jr $ra
+	subiu $sp $sp 12
+	sw $ra 0($sp)
+	sw $a0 4($sp)
+	sw $a1 8($sp)
+
+	move $a0, $t2
+	li $v0, 14
+	la $a1, buffer
+	syscall  
+	la $a0, buffer
+	li $v0, 4
+	syscall
+
+	lw $ra 0($sp)
+	lw $a0 4($sp)
+	lw $a1 8($sp)
+	addiu $sp $sp 12
+	jr $ra
 
 exit:
 li $v0 10
