b2b8d7593c187bbdc80ee3ecf8ae60b14aeb9df5
Théo Poizat <theo.poizat@etu.unistra.fr>
2015-12-07 21:25:30 +0100

    Indentation

--- compression.s
+++ compression.s
@@ -8,134 +8,133 @@
   cre: .asciiz "./test.lz77" # nom du fichier ecrit
 
 .text
-# N=6 F=5
-li $t0 6
-li $t1 5
+  # N=6 F=5
+  li $t0 6
+  li $t1 5
 
-#### DEBUT Ouverture et lecture du fichier
-li $v0 13
-la $a0, nom_fichier
-li $a1, 0 # ouverture pour écriture
-li $a2, 0
-syscall
+  #### DEBUT Ouverture et lecture du fichier
 
-# On stocke le descripteur du fichier dans t2
-move $t2, $v0
+  li $v0 13
+  la $a0, nom_fichier
+  li $a1, 0 # ouverture pour écriture
+  li $a2, 0
+  syscall
 
-move $a0, $t2
-li $v0, 14
-la $a1, buffer
-li $a2, 1050 # taille du buffer en dur
-syscall
+  # On stocke le descripteur du fichier dans t2
+  move $t2, $v0
 
-#### FIN Ouverture et lecture du fichier
+  move $a0, $t2
+  li $v0, 14
+  la $a1, buffer
+  li $a2, 1050 # taille du buffer en dur
+  syscall
 
-jal create
+  #### FIN Ouverture et lecture du fichier
 
-jal CreerTampon
+  jal create
 
-j exit
+  jal CreerTampon
 
-#### DEBUT Fonction de creation tampon
-CreerTampon:
-  subiu $sp $sp 20
-  sw $ra 0($sp)
-  sw $a0 4($sp)
-  sw $a1 8($sp)
-  sw $a2 12($sp)
-  sw $s1 16($sp)
+  j exit
 
-  li $s1 0
+  #### DEBUT Fonction de creation tampon
+  CreerTampon:
+    subiu $sp $sp 20
+    sw $ra 0($sp)
+    sw $a0 4($sp)
+    sw $a1 8($sp)
+    sw $a2 12($sp)
+    sw $s1 16($sp)
 
-  add $a1 $t0 $t1
-  RappelTampon:
-    jal Extraction
-    addi $s1 $s1 1
-	addi $t4 $t4 1
-    bne $s1 $a1 RappelTampon
+    li $s1 0
 
-  lw $ra 0($sp)
-  lw $a0 4($sp)
-  lw $a1 8($sp)
-  lw $a2 12($sp)
-  lw $s1 12($sp)
-  addiu $sp $sp 20
-  jr $ra
+    add $a1 $t0 $t1
+    RappelTampon:
+      jal Extraction
+      addi $s1 $s1 1
+      addi $t4 $t4 1
+      bne $s1 $a1 RappelTampon
 
-# Recupere le 
-Extraction:
-	la $t5, buffer
-	add $t5 $t5 $s1
-	lb $a0, 0($t5)
-	jr $ra
+    lw $ra 0($sp)
+    lw $a0 4($sp)
+    lw $a1 8($sp)
+    lw $a2 12($sp)
+    lw $s1 12($sp)
+    addiu $sp $sp 20
+    jr $ra
 
+  # Recupere le 
+  Extraction:
+    la $t5, buffer
+    add $t5 $t5 $s1
+    lb $a0, 0($t5)
+    jr $ra
 
+  # Creer un fichier avec le nom cre
+  create:
+    subiu $sp $sp 16
+    sw $ra 0($sp)
+    sw $a0 4($sp)
+    sw $a1 8($sp)
+    sw $a2 12($sp)
 
-# Creer un fichier avec le nom cre
-create:
-	subiu $sp $sp 16
-	sw $ra 0($sp)
-	sw $a0 4($sp)
-	sw $a1 8($sp)
-	sw $a2 12($sp)
+    li $v0, 13
+    la $a0, cre
+    li $a1, 1
+    li $a2, 0
+    syscall
+    move $t3, $v0
 
-	li $v0, 13
-	la $a0, cre
-	li $a1, 1
-	li $a2, 0
-	syscall
-	move $t3, $v0
+    lw $ra 0($sp)
+    lw $a0 4($sp)
+    lw $a1 8($sp)
+    lw $a2 12($sp)
+    addiu $sp $sp 16
+    jr $ra
 
-	lw $ra 0($sp)
-	lw $a0 4($sp)
-	lw $a1 8($sp)
-	lw $a2 12($sp)
-	addiu $sp $sp 16
-	jr $ra
+  # Ecrit dans le fichier lz77 les 10 caracteres dans $s1
+  write:
+    subiu $sp $sp 16
+    sw $ra 0($sp)
+    sw $a0 4($sp)
+    sw $a1 8($sp)
+    sw $a2 12($sp)
 
-# Ecrit dans le fichier lz77 les 10 caracteres dans $s1
-write:
-	subiu $sp $sp 16
-	sw $ra 0($sp)
-	sw $a0 4($sp)
-	sw $a1 8($sp)
-	sw $a2 12($sp)
+    li $v0, 15
+    move $a0, $t3
+    la $a1, ($s1)
+    li $a2, 10
+    syscall
 
-	li $v0, 15
-	move $a0, $t3
-	la $a1, ($s1)
-	li $a2, 10
-	syscall
+    lw $ra 0($sp)
+    lw $a0 4($sp)
+    lw $a1 8($sp)
+    lw $a2 12($sp)
+    addiu $sp $sp 16
+    jr $ra
 
-	lw $ra 0($sp)
-	lw $a0 4($sp)
-	lw $a1 8($sp)
-	lw $a2 12($sp)
-	addiu $sp $sp 16
-	jr $ra
+  # Ferme les fichiers dans $t2 et $t3
+  close:
+    subiu $sp $sp 16
+    sw $ra 0($sp)
+    sw $a0 4($sp)
+    sw $a1 8($sp)
+    sw $a2 12($sp)
 
-# Ferme les fichiers dans $t2 et $t3
-close:
-	subiu $sp $sp 16
-	sw $ra 0($sp)
-	sw $a0 4($sp)
-	sw $a1 8($sp)
-	sw $a2 12($sp)
+    li   $v0, 16
+    move $a0, $t2
+    syscall
+    li   $v0, 16
+    move $a0, $t3
+    syscall
 
-	li   $v0, 16
-	move $a0, $t2
-	syscall
-	li   $v0, 16
-	move $a0, $t3
-	syscall
+    lw $ra 0($sp)
+    lw $a0 4($sp)
+    lw $a1 8($sp)
+    lw $a2 12($sp)
+    addiu $sp $sp 16
+    jr $ra
 
-	lw $ra 0($sp)
-	lw $a0 4($sp)
-	lw $a1 8($sp)
-	lw $a2 12($sp)
-	addiu $sp $sp 16
-	jr $ra
-
-exit:
-li $v0 10
-syscall
+  Exit:
+    li $v0 10
+    syscall
