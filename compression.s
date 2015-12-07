edbc20f1c6762e6ebe8119ee550d9945e7dc3711
Théo Poizat <theo.poizat@etu.unistra.fr>
2015-12-07 15:47:27 +0100

    Ajout de la fonction CreerTampon

--- compression.s
+++ compression.s
@@ -1,18 +1,18 @@
 .data
-nom_fichier: .asciiz "./Lepetitprince.txt" # nom du fichier
+  nom_fichier: .asciiz "./Lepetitprince.txt" # nom du fichier
 
-saut_ligne: .asciiz "\n"
-toast: .asciiz "\n------------\n"
+  saut_ligne: .asciiz "\n"
+  toast: .asciiz "\n------------\n"
 
-buffer: .space 1050
-cre: .asciiz "./test.lz77" # nom du fichier ecrit
+  buffer: .space 1050
+  cre: .asciiz "./test.lz77" # nom du fichier ecrit
 
 .text
-#N=11 F=5
-li $t0 11
+# N=6 F=5
+li $t0 6
 li $t1 5
 
-# Ouverture du fichier
+#### DEBUT Ouverture et lecture du fichier
 li $v0 13
 la $a0, nom_fichier
 li $a1, 0 # ouverture pour écriture
@@ -21,48 +21,62 @@ syscall
 
 # On stocke le descripteur du fichier dans t2
 move $t2, $v0
-move $a0, $v0
 
-# Affichage 1
-li $a2 50
-jal Lecture
+move $a0, $t2
+li $v0, 14
+la $a1, buffer
+li $a2, 1050 # taille du buffer en dur
+syscall
 
-la $a0, toast
-li $v0,4
+#### FIN Ouverture et lecture du fichier
+
+jal create
+
+jal CreerTampon
+
+move $a0 $t4
+li $v0 4
 syscall
 
 j exit
 
-# Lecture d'un fichier
-# a2 la taille
-Lecture:
-	subiu $sp $sp 12
-	sw $ra 0($sp)
-	sw $a0 4($sp)
-	sw $a1 8($sp)
+#### DEBUT Fonction de creation tampon
+CreerTampon:
+  subiu $sp $sp 20
+  sw $ra 0($sp)
+  sw $a0 4($sp)
+  sw $a1 8($sp)
+  sw $a2 12($sp)
+  sw $s1 16($sp)
 
-	li $v0, 14
-	la $a1, buffer
-	syscall
+  li $s1 0
 
-	la $a0, buffer
-	li $v0, 4
-	syscall
+  la $a2 ($t4)
 
-	la $a0, toast
-	li $v0,4
-	syscall
-	#C'EST PAR LA QUE CA SE PASSE
+  add $a1 $t0 $t1
+  RappelTampon:
+    jal Extraction
+    move $a2 $a0
+    addi $s1 $s1 1
+    addi $a2 $a2 1
+    bne $s1 $a1 RappelTampon
+
+  lw $ra 0($sp)
+  lw $a0 4($sp)
+  lw $a1 8($sp)
+  lw $a2 12($sp)
+  lw $s1 12($sp)
+  addiu $sp $sp 20
+  jr $ra
+
+# Recupere le 
+Extraction:
 	la $t0, buffer
-	lb $a0, 0($t0)#REMPLACE LE ZERO PAR LA POSITION DE LA LETTRE SOUHAITER
-	li $v0, 11
-	syscall
-
-	lw $ra 0($sp)
-	lw $a0 4($sp)
-	lw $a1 8($sp)
-	addiu $sp $sp 12
+	add $t0 $t0 $s1
+	lb $a0, 0($t0)
 	jr $ra
+
+
 
 # Creer un fichier avec le nom cre
 create:
